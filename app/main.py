9c8d268e8a2e92e719c3fc58980e18f861b005a2
Elliot Bohlen <eebohlen@gmail.com>
2020-09-29 19:23:33 -0400

    more text

--- app/main.py
+++ app/main.py
@@ -7,13 +7,13 @@ with open("elliot.txt") as f:
     text = f.read()
 
 # Build the model.
-text_model = markovify.NewlineText(text)
+text_model = markovify.Text(text)
 
 #NewlineText
 #retain_original=False
 # Print three randomly-generated sentences of no more than 280 characters
-for i in range(3):
-    message = text_model.make_short_sentence(280, state_size=2)
+for i in range(1):
+    message = text_model.make_short_sentence(300, state_size=3, retain_original=False)
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -22,7 +22,7 @@ api = tweepy.API(auth)
 
 api.update_status(message)
 print(message)
-print("donneeee")
+
 
 #docker build . --tag project1:1.0
 # docker run --rm -it -v "$(pwd)/app:/app" project1:1.0
